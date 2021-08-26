b53719fda5f4343c4b97db26c6b547121c4a1c62
zhanghao <351571555@qq.com>
2021-08-26 22:15:40 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    music.playMelody("C5 B A G F E D C ", 120)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+})
